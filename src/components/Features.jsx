7ea56cf52c9063045cb054b34d199fc5160e8bd8
BlackSploit <blacksploitx0@gmail.com>
2024-12-28 21:38:44 +0530

    msg

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,8 +12,6 @@ import mom5 from '../../beebrozz/mom4.png'
 import mom6 from '../../beebrozz/mom5.png'
 import mom7 from '../../beebrozz/mom6.png'
 
-
-
 const Features = () => {
   const videoRef = useRef();
 
@@ -49,108 +47,104 @@ const Features = () => {
     });
   }, []);
 
+  const handleWhatsAppClick = (momentNumber) => {
+    const message = `Hi, I would like more information about Momento ${momentNumber}. Please share more details.`;
+    const encodedMessage = encodeURIComponent(message);
+    window.open(`https://wa.me/+919633963434?text=${encodedMessage}`, "_blank");
+  };
+
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-width">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading font-light">
-          Beebroz in Action
+            Beebroz in Action
           </h1>
         </div>
 
         <div className="flex flex-col justify-center items-center overflow-hidden">
-     
-
           <div className="flex-center flex-col sm:px-10">
-           
-
             <div className="flex flex-col w-full relative">
               <div className="feature-video-container">
                 <div className="overflow-hidden flex-1 h-[100vh]">
                   <img
                     src={mom1}
-                    alt="titanium"
+                    alt="Momento 1"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(1)}
                   />
                 </div>
 
                 <div className="overflow-hidden flex-1 h-[60vh]">
                   <img
                     src={mom2}
-                    alt="titanium2"
+                    alt="Momento 2"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(2)}
                   />
                 </div>
               </div>
-
-
-
 
               <div className="feature-video-container">
                 <div className="overflow-hidden flex-1 h-[100vh]">
                   <img
                     src={mom6}
-                    alt="titanium"
+                    alt="Momento 6"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(6)}
                   />
                 </div>
 
                 <div className="overflow-hidden flex-1 h-[60vh]">
                   <img
                     src={mom7}
-                    alt="titanium2"
+                    alt="Momento 7"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(7)}
                   />
                 </div>
               </div>
 
-
-
               <div className="feature-video-container">
-
-
-              <div className="overflow-hidden flex-1 h-[100vh]">
+                <div className="overflow-hidden flex-1 h-[100vh]">
                   <img
                     src={mom4}
-                    alt="titanium2"
+                    alt="Momento 4"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(4)}
                   />
                 </div>
-
 
                 <div className="overflow-hidden flex-1 h-[100vh]">
                   <img
                     src={mom5}
-                    alt="titanium"
+                    alt="Momento 5"
                     className="feature-video g_grow"
+                    onClick={() => handleWhatsAppClick(5)}
                   />
                 </div>
-
-               
               </div>
 
-
-
-
               <div className="feature-text-container">
-  <div className="flex-1 flex-center">
-    <p className="feature-text g_text">
-      At Beebroz, we specialize in{" "}
-      <span className="text-white">crafting unique and innovative designs</span>
-      , combining creativity and technology to bring your vision to life.
-    </p>
-  </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    At Beebroz, we specialize in{" "}
+                    <span className="text-white">
+                      crafting unique and innovative designs
+                    </span>
+                    , combining creativity and technology to bring your vision to life.
+                  </p>
+                </div>
 
-  <div className="flex-1 flex-center">
-    <p className="feature-text g_text">
-      Our team focuses on{" "}
-      <span className="text-white">high-quality, user-centered design</span>{" "}
-      that enhances both functionality and aesthetics. You'll see the difference
-      in every project we deliver.
-    </p>
-  </div>
-</div>
-
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Our team focuses on{" "}
+                    <span className="text-white">high-quality, user-centered design</span>{" "}
+                    that enhances both functionality and aesthetics. You'll see the difference
+                    in every project we deliver.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
